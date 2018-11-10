457593573bd2b1bc79e846a5298f6598555868c2
Ashish <ashishranjan30@gmail.com>
2018-11-10 17:40:10 +0530

    Fixed Quick Sort Algo and added program to check if a pair with sum exists

--- src/org/home/prac/Arrays/PairWithGivenSum.java
+++ src/org/home/prac/Arrays/PairWithGivenSum.java
@@ -3,9 +3,20 @@ package org.home.prac.Arrays;
 public class PairWithGivenSum {
 
 	private static boolean hasPairWithGivenSum(int arr[], int arraySize, int sum) {
-		
 		//Sorting the Array
 		quickSort(arr,  0, arraySize -1);
+
+		int startIndex = 0;
+		int endIndex = arraySize -1;
+		while (endIndex > startIndex){
+			if(arr[startIndex]+arr[endIndex] ==  sum) {
+				return true;
+			} else if (arr[startIndex]+arr[endIndex] > sum) {
+				endIndex--;
+			} else {
+				startIndex++;
+			}
+		}
 		return false;
 	}
 	
@@ -19,12 +30,9 @@ public class PairWithGivenSum {
 	}
 	
 	private static int partition(int[] arr, int startIndex, int endIndex) {
-		
-		System.out.println("Picking "+arr[endIndex]+ " as pivot");
 		int pivot = arr[endIndex];
 		int i = startIndex - 1;
-		for(int j = 0; j < endIndex; j++) {
-			System.out.println("startIndex "+startIndex+" endIndex "+endIndex+" i "+i+" j "+j);
+		for(int j = startIndex; j < endIndex; j++) {
 			// if current element is smaller or equal to pivot;
 			if(arr[j] <= pivot) {
 				i++;
@@ -34,29 +42,22 @@ public class PairWithGivenSum {
 				arr[j] = temp; 
 			}
 		}
-		printArray(arr);
-		System.out.println("Swapping Pivot"+pivot+" with position i "+i);
 		//Swapping pivot element with i+1
 		arr[endIndex] = arr[i+1];
 		arr[i+1] = pivot;
 		
-		printArray(arr);
 		return i+1;
-	}
-	
-	private static void printArray(int[] arr) {
-		for(int i: arr) {
-			System.out.print("\t"+i);
-		}
-		System.out.println();
 	}
 	
 	public static void main(String[] args) {
 		//int[] arr0 = {5, 6, 11, 65, 6, -1, 6};
-		int arr1[] = {10, 7, 8, 9, 1, 5};
-		
-		quickSort(arr1, 0, arr1.length - 1);
-		
-		System.out.println(arr1.toString());
+		int arr[] = {1, 4, 45, 6, 10, -8}; 
+		int sum = 16; 
+		int arraySize = 6;
+		if(hasPairWithGivenSum(arr, arraySize, sum)) {
+			System.out.println("There is a pair with sum as "+sum);
+		} else {
+			System.out.println("There are no pair with sum as "+sum);
+		}
 	}
 }
